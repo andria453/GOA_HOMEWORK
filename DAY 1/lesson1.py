6e51e387f279630aa0554c2b4e38169c41476341
andria453 <andriakobidze@gmail.com>
2024-10-04 22:48:31 +0400

    first commit

---
+++ DAY 1/lesson1.py
@@ -0,0 +1,66 @@
+from turtle import *
+#painting a house
+width(7)
+color("blue")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+#drawing a door
+forward(70)
+color("red")
+left(90)
+forward(100)
+right(90)
+forward(56)
+right(90)
+forward(100)
+left(90)
+
+penup()
+goto(200, 200)
+pendown()
+
+
+left(120)
+forward(200)
+left(120)
+forward(200)
+
+
+
+left(120)
+forward(200)
+
+
+penup()
+goto(125, 125)
+pendown()
+
+forward(50)
+left(90)
+forward(60)
+left(90)
+forward(50)
+left(90)
+forward(60)
+
+penup()
+goto(25,125)
+pendown()
+
+left(90)
+forward(50)
+left(90)
+forward(60)
+left(90)
+forward(50)
+left(90)
+forward(52)
+
+exitonclick()
