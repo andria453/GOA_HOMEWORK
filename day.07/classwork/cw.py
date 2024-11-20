be524117dd45c88edd98fb5cc219597e689d283d
andria453 <andriakobidze@gmail.com>
2024-11-20 16:08:46 +0400

    davaleba

---
+++ day.07/classwork/cw.py
@@ -0,0 +1,18 @@
+#momxmareblis saxeli da paroli 
+name = input("შეიყვანე შენი სახელი: ")
+password = input("შეიყვანე შენი პაროლი: ")
+print("გამარჯობა", name  , "შენი პაროლია " , password )
+
+#matematikuri moqmedebebi 
+number = int(input("შეიყვანე რიცხვი:" ))
+num2 = int(input("შეიყვანე რიცხვი:" ))
+print(number + num2 )
+print(number - num2 )
+print( number * num2)
+print(number / num2)
+
+
+
+
+
+
---
+++ day.07/homework/hm.py
@@ -0,0 +1,35 @@
+#momxmareblis monacemebi.
+name = input("შეიყვანე შენი სახელი: ")
+surname = input("შეიყვანე შენი გვარი: ")
+age = input("შეყვანე შენი ასაკი:")
+print("hello" , name + surname)
+print("how are you")
+
+
+#matematikuri moqmedebebi.
+num1 = int(input("შეიყვანე ერთი რიცვი:"))
+num2 = int(input("შეიყვანე მეორე რიცხვი:"))
+print(num1 * num2)
+print(num1 + num2)
+print(num1 / num2)
+print(num1 - num2)
+
+#aq aris momxmarebli e-maili da paroli.
+email = input("your e-mail")
+password=input("your password")
+print("your e-mail is" , email ) 
+print("your password is:" , password )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
