6100350c1dd0e103f41f84ab3fc892e32c95bc78
andria453 <andriakobidze@gmail.com>
2024-12-12 20:25:02 +0400

    good

---
+++ day.09/claswork/cw.py
@@ -0,0 +1,17 @@
+num1=float(input())
+num2=float(input())
+print(num1/num2)
+print(num1*num2)
+print(num1+num2)
+print(num1-num2)
+
+
+
+#concatenation გვეხმაება იმაში რომ ერთ სტრინგს მივუმატოთ მეორე ანუ ორ სტინგს აერთიანებს
+# მაგ: "hello" + "world" = "helloworld"
+
+
+
+
+#int- არის მთელი რიცხვები ანუ აქცევს სტრინგებს მთელ რიცვებად
+#flout კი არის ათწილადი
---
+++ day.09/homework/hm.py
@@ -0,0 +1,15 @@
+int(input("შეიყვანე დაბადების წელი"))
+#მე ვაქცის ეს int ეჯერად რადგან გადამექცია მთელ რიცხვად
+
+
+name=input("შენი სახელი")
+
+print( "hello" + name  + "!")
+   
+#concatenation გვეხმაება იმაში რომ ერთ სტრინგს მივუმატოთ მეორე ანუ ორ სტინგს აერთიანებს
+# მაგ: "hello" + "world" = "helloworld"
+
+
+# int , str, float
+
+#type() viyenebt sheuzlia ori tipis argumenti gaaertianos. str aris stringebi . int ki mteli ricvebi.
